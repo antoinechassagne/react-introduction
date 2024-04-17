4d2e27e82387844dedf93619330699c6949b4d9f
Antoine Chassagne <antoine@poool.fr>
2024-04-17 13:05:21 +0200

    feat: add unique id to persons

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,31 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 
-import Title from "./components/Title";
-import PersonsList from "./components/PersonsList";
-import PersonNameInput from "./components/PersonNameInput";
-import Button from "./components/Button";
+import Title from './components/Title';
+import PersonsList from './components/PersonsList';
+import PersonNameInput from './components/PersonNameInput';
+import Button from './components/Button';
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Alice" }]);
-  const [newName, setNewName] = useState("");
+  const [persons, setPersons] = useState([
+    { id: crypto.randomUUID(), name: 'Alice' }
+  ]);
+  const [newName, setNewName] = useState('');
 
   useEffect(() => {
-    console.log("State persons: ", persons);
+    console.log('State persons: ', persons);
   }, [persons]);
+
+  const addPerson = () => {
+    const person = { id: crypto.randomUUID(), name: newName };
+    setPersons([...persons, person]);
+  }
 
   return (
     <div>
       <Title>Persons list</Title>
       <PersonsList persons={persons} />
       <PersonNameInput onChange={(e) => setNewName(e.target.value)} />
-      <Button onClick={() => setPersons([...persons, { name: newName }])}>
+      <Button onClick={addPerson}>
         Add person
       </Button>
     </div>
