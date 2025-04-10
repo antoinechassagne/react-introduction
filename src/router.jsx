1563ea78842bc2e908e0227f33f88ca40b8c8d12
Antoine Chassagne <antoine@poool.fr>
2025-04-10 04:27:30 +0200

    fix: use react-router

--- src/router.jsx
+++ src/router.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter } from "react-router";
 
 import Hello from "./pages/Hello";
 import Goodbye from "./pages/Goodbye";
